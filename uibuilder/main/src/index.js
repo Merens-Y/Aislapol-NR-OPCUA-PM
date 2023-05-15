3b5269ebcbb2072ba3edfe5a4e71708a17939c55
Merens-Y <58517637+Merens-Y@users.noreply.github.com>
2023-05-15 17:27:03 -0400

    component based rendering

--- uibuilder/main/src/index.js
+++ uibuilder/main/src/index.js
@@ -1,6 +1,4 @@
 // @ts-nocheck
-
-
 'use strict'
 const API_URL = 'http://127.0.0.1:1880';
 const options = {
@@ -26,6 +24,135 @@ const options = {
  * See the docs if the client doesn't start on its own.
  */
 
+Vue.component('login-form', {
+    // Component options: template, data, methods, etc.
+    template:`
+    `,
+    data () {
+        return{}
+    },
+    methods: {},
+});
+
+Vue.component('main-data', {
+    props: ['pre_exp_arr', 'mold_arr'],
+    template:`
+    <div class="col-xl">
+        <button class="p-3" @click="toggleMachines">Estado de Maquinarias</button>
+        <button class="p-3" @click="toggleDatabase">Base de Datos</button>
+        <button class="p-3" @click="toggleMonitor">Monitor de Variables productivas</button>
+        <b-card v-if="showMachines">
+            <div>{{ cacheReplay() }}</div>
+            <h3 slot="header">Estado de Maquinarias</h3>
+            <div class="row">
+                <div class="card col-lg" v-for="pre_exp in pre_exp_arr" v-bind:class="[pre_exp.estado.texto]">
+                        <div class="row">
+                            <p class="col">Pre-Expansor {{pre_exp.numero}}</p> <div class="col-" v-bind:class="[pre_exp.estado.led]"></div>
+                            <div class="w-100"></div>
+                            <div class="col-sm"><p><b>Material:</b></p></div>                                
+                            <div class="col-md value-field text-center">{{pre_exp.material}}</div>
+                            <div class="w-100"></div>
+                            <div class="col-sm"><p><b>Kg Procesados:</b></p></div>                                
+                            <div class="col-md value-field text-center">{{pre_exp.kilog}}</div>
+                            <div class="w-100"></div>
+                            <div class="col-sm"><p><b>Kg Procesados:</b></p></div>                                
+                            <div class="col-md value-field text-center">{{pre_exp.kilog}}</div>
+                            <div class="w-100"></div>
+                            <div class="col-sm"><p><b>Tiempo:</b></p></div>                                
+                            <div class="col-md value-field text-center">{{pre_exp.tiempo}}</div>
+                            <div class="w-100"></div>
+                            <div class="col-sm"><p><b>Densidad:</b></p></div>                                
+                            <b-col class="col-md value-field text-center">
+                                <b-row class="no-gutters">
+                                    <div class="col">{{pre_exp.densidad.min}}<br>min</div>
+                                    <div class="col value-field">{{pre_exp.densidad.avg}}<br>prom</div>
+                                    <div class="col value-field">{{pre_exp.densidad.max}}<br>max</div>
+                                </b-row>
+                                
+                            </b-col>
+                            <div class="w-100"></div>
+                            <b-col class="p-3"></b-col> 
+                        </div>                                                             
+                    
+                </div>
+                <div class="card col-md" v-for="mold in mold_arr" v-bind:class="[mold.estado.texto]">
+                    <div class="row">
+                        <p class="col">Moldeador {{mold.numero}}</p> <div class="col-" v-bind:class="[mold.estado.led]"></div>
+                        <div class="w-100"></div>
+                        <div class="col-sm"><p><b>Molde:</b></p></div>
+                        <div class="col-md value-field text-center">{{mold.molde}}</div>
+                        <div class="w-100"></div>
+                        <div class="col-sm"><p><b>Ciclos por Hora:</b></p></div>
+                        <div class="col-md value-field text-center">{{mold.ciclos_hora}}</div>
+                        <div class="w-100"></div>
+                        <div class="col-sm"><p><b>Tiempo de Ciclo:</b></p></div>
+                        <div class="col-md value-field text-center">{{mold.tiempo_ciclo}}</div>
+                        <div class="w-100"></div>
+                        <div class="col-sm"><p><b>Total de ciclos:</b></p></div>
+                        <div class="col-md value-field text-center">{{mold.ciclos_total}}</div>                                                       
+                    </div>
+                </div>
+            </div>
+        </b-card>
+        <b-card v-if="showDatabase">
+            <h3 slot="header">Base de Datos</h3>
+            <b-form-input id="type-search" placeholder="Ingresar Query"></b-form-input>
+            <b-table striped hover :items="tableData"></b-table>
+        </b-card>
+        <b-card v-if="showMonitor">
+            <h3 slot="header">Monitor de Variables Productivas</h3>
+        </b-card>
+    </div>
+    `,
+    data () {
+        return{
+            imgProps: { width: 75, height: 75 },
+            showMachines: false,
+            showDatabase: false,
+            showMonitor: false,
+            showHomepage: true,
+            tableData: [
+                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+                { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+                { age: 38, first_name: 'Jami', last_name: 'Carney' }
+            ],
+        }
+    },
+    methods: {
+        cacheReplay: function(){
+            uibuilder.send({payload: "Hi there from the client", topic: "from the client", cacheControl: "REPLAY"});
+        },
+
+        toggleMachines: function(){
+            if(this.showMachines===false){
+                this.showMachines = true;
+                this.showHomepage = false;
+                this.showDatabase = false;
+                this.showMonitor = false;
+            }
+        },
+
+        toggleDatabase: function(){
+            if(this.showDatabase===false){
+                this.showMachines = false;
+                this.showHomepage = false;
+                this.showDatabase = true;
+                this.showMonitor = false;
+            }
+        },
+
+        toggleMonitor: function(){
+            if(this.showMonitor===false){
+                this.showMachines = false;
+                this.showHomepage = false;
+                this.showDatabase = false;
+                this.showMonitor = true;
+            }
+        },
+    },
+});
+
 const app = new Vue({ // eslint-disable-line no-unused-vars
     el: '#app',
 
@@ -41,6 +168,8 @@ const app = new Vue({ // eslint-disable-line no-unused-vars
         isPasswordValid: null,
         passwordFeedback: 'Ingresa una contraseña válida',
         userToken: '',
+        pre_exp_arr: [{},{}],
+        mold_arr: [{},{}],
 
     } }, // --- End of data --- //
     mounted() {
