20974c5aeacc5fd681e54d34ad44b6435a81304d
Merens-Y <58517637+Merens-Y@users.noreply.github.com>
2023-06-19 11:51:21 -0400

    nuevo modal de informació como placeholder.

--- uibuilder/main/src/components/machineData.js
+++ uibuilder/main/src/components/machineData.js
@@ -14,7 +14,7 @@ export default {
     <b-container fluid>
         <div class="row">
             <div>{{ triggerCacheReplay() }}</div>
-            <b-card style="cursor: pointer;" class="col-12 col-lg-5 col-xl-4 p-1 m-1" v-for="pre_exp in pre_exp_arr" v-bind:class="[pre_exp.estado.texto]" @click="info(pre_exp, $event.target)">
+            <b-card style="cursor: pointer;" class="col-12 col-lg-5 col-xl-4 p-1 m-1" v-for="pre_exp in pre_exp_arr" v-bind:class="[pre_exp.estado.texto]" @click="preexpInfo(pre_exp)">
                 <div class="row">
                     <p class="col p-1 m-1">Pre-Expansor {{pre_exp.numero}}</p> <div class="col-" v-bind:class="[pre_exp.estado.led]"></div>
                     <div class="w-100"></div>
@@ -37,7 +37,7 @@ export default {
                     </b-col>
                 </div>
             </b-card>
-            <b-card style="cursor: pointer;" class="col-12 col-md-5 col-lg-3 p-1 m-1" v-for="mold in mold_arr" v-bind:class="[mold.estado.texto]" @click="info(mold, $event.target)">
+            <b-card style="cursor: pointer;" class="col-12 col-md-5 col-lg-3 p-1 m-1" v-for="mold in mold_arr" v-bind:class="[mold.estado.texto]" @click="moldInfo(mold)">
                 <div class="row">
                     <p class="col p-1 m-1">Moldeador {{mold.numero}}</p> <div class="col-" v-bind:class="[mold.estado.led]"></div>
                     <div class="w-100"></div>
@@ -56,8 +56,76 @@ export default {
             </b-card>
         </div>
         <!-- Info modal -->
-        <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
-            <div v-html="infoModal.content"></div>
+        <b-modal size="xl" :id="moldModal.id" :title="moldModal.title" >
+            <b-row>
+              <b-col lg="2" class="my-1"><b>Fecha:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Producto:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Turno:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Encargado de Turno:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Operador:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Inicio de Ciclo:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Término de Ciclo:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Lote:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Código de Molde:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Horómetro:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Horómetro Manual:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Horómetro Total:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+            </b-row>
+            <template #modal-footer="{ ok, cancel, hide }">
+                <!-- Emulate built in modal footer ok and cancel button actions -->
+                <b-button size="sm" variant="primary" @click="ok()">
+                  OK
+                </b-button>
+            </template>
+        </b-modal>
+        <b-modal size="xl" :id="preexpModal.id" :title="preexpModal.title" >
+            <b-row>
+              <b-col lg="2" class="my-1"><b>Fecha:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Producto:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Encargado de Turno:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Operador:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Cantidad Producida:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Tiempo de Inicio:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Tiempo de Término:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Espumado total (Kg):</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Número de Silo:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Materia Prima:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Número de Lote de Materia Prima:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Densidad Media:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              <b-col lg="2" class="my-1"><b>Tiempo de descanso Silo:</b></b-col>
+              <b-col lg="2" class="my-1 value-field text-center">Placeholder</b-col>
+              
+            </b-row>
+            <template #modal-footer="{ ok, cancel, hide }">
+                <!-- Emulate built in modal footer ok and cancel button actions -->
+                <b-button size="sm" variant="primary" @click="ok()">
+                  OK
+                </b-button>
+            </template>
         </b-modal>
     </b-container>
     `,
@@ -68,8 +136,13 @@ export default {
       mold_cards: [],
 
       // variables for controlling the display of the info modal.
-      infoModal: {
-        id: "info-modal",
+      moldModal: {
+        id: "mold-Modal",
+        title: "",
+        content: "",
+      },
+      preexpModal: {
+        id: "preexp-Modal",
         title: "",
         content: "",
       },
@@ -85,41 +158,15 @@ export default {
     triggerCacheReplay() {
       this.$emit("cachereplay-event");
     },
-    info(machine_obj, card) {
-      this.infoModal.title = `Máquina numero: ${machine_obj.numero}`;
-      this.infoModal.content = `
-        <b-card class="col-12 col-lg-5 col-xl-4 p-1 m-1">
-            <div class="row">
-                <p class="col p-1 m-1">Pre-Expansor </p> <div class="col- led green"></div>
-                <div class="w-100"></div>
-                <div class="col-sm"><p><b>Material: name</b></p></div>                                
-                <div class="col-md value-field text-center">value</div>
-                <div class="w-100"></div>
-                <div class="col-sm"><p><b>Kg Procesados:</b></p></div>                                
-                <div class="col-md value-field text-center">value</div>
-                <div class="w-100"></div>
-                <div class="col-sm"><p><b>Tiempo:</b></p></div>                                
-                <div class="col-md value-field text-center">value</div>
-                <div class="w-100"></div>
-                <div class="col-sm"><p><b>Densidad:</b></p></div>                                
-                <b-col class="col-md value-field text-center">
-                    <b-row class="no-gutters">
-                        <div class="col">value<br>min</div>
-                        <div class="col">value<br>prom</div>
-                        <div class="col">value<br>max</div>
-                    </b-row>
-                </b-col>
-                <div class="w-100"></div>
-                <b-col class="p-3"></b-col> 
-            </div>
-        </b-card>
-      `;
-      //   this.infoModal.content = JSON.stringify(machine_obj, null, 2);
-      this.$root.$emit("bv::show::modal", this.infoModal.id, card);
+    moldInfo(machine_obj) {
+      this.moldModal.title = `Moldeadora numero: ${machine_obj.numero}`;
+      this.$root.$emit("bv::show::modal", this.moldModal.id);
+      this.content = machine_obj;
     },
-    resetInfoModal() {
-      this.infoModal.title = "";
-      this.infoModal.content = "";
+    preexpInfo(machine_obj) {
+      this.preexpModal.title = `Pre-expansor numero: ${machine_obj.numero}`;
+      this.$root.$emit("bv::show::modal", this.preexpModal.id);
+      this.content = machine_obj;
     },
     getVariantClassbyNC(cycleTime, moldType) {
       if (this.mold_control_values.length === 0 || this.mold_control_values.find((obj) => obj.mold_name === moldType) === undefined) {
